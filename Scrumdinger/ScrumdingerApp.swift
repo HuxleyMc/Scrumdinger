c49f2d146f681ff2a0f99d99eb625f24c5c64f02
HuxleyMc <huxhales@gmail.com>
2021-07-14 13:46:20 +1000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Huxley McGuffin on 14/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
